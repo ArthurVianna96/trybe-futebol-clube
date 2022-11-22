02f5323e469a0fdcf371cb71105daeca9d80e33e
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-11-22 18:49:04 -0300

    fixed docker issue

--- app/backend/src/database/migrations/20221122204008-matches-table.js
+++ app/backend/src/database/migrations/20221122204008-matches-table.js
@@ -12,24 +12,35 @@ module.exports = {
       homeTeam: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        foreignKey: true,
+        field: 'home_team',
+        references: {
+          key: 'id',
+          model: 'teams',
+        },
       },
       homeTeamGoals: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        field: 'home_team_goals'
       },
       awayTeam: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        foreignKey: true,
+        field: 'away_team',
+        references: {
+          key: 'id',
+          model: 'teams',
+        },
       },
       awayTeamGoals: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        field: 'away_team_goals'
       },
       inProgress: {
         type: Sequelize.BOOLEAN,
         allowNull: false,
+        field: 'in_progress'
       }
     });
   },
